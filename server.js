05476bc38714433b536377358107c18bee8aa3da
Yahkob <JakobHarclerode@gmail.com>
2016-07-25 12:55:45 -0700

    add response handler

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var router = express.Router();
-var Post = require('./models/post.js');
+let express = require('express');
+let app = express();
+let mongoose = require('mongoose');
+let router = express.Router();
+let Post = require('./models/post.js');
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.use('/api', router);
+
 
 mongoose.connect('mongodb://localhost/chatDB');
 
@@ -17,12 +21,12 @@ db.once('open', function () {
 
 router.post('/chat/newMsg', function (req, res) {
     let body = req.body;
-
     let post = Post({
         author: body.author,
         post: body.post
     });
     post.save();
+    res.end('{"success" : "Posted Successfully", "status" : 200}');
 });
 
 router.get('/chat/messages', function (req, res) {
