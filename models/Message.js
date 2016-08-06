83933c23620fd367991de99708e10795d99b1eaf
Yahkob <JakobHarclerode@gmail.com>
2016-08-06 14:23:02 -0700

    change bodyparser to use urlencoded format, change Post to Message refactor functions to arrow syntax, handle cors

--- models/Message.js
+++ models/Message.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var postSchema = new Schema({
+var messageSchema = new Schema({
     date: {type: Date, default: Date.now},
     author: {type: String, default: 'Guest'},
     post: String
 });
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model('Message', messageSchema);
--- server.js
+++ server.js
@@ -4,52 +4,46 @@ const port = process.env.PORT || 3000;
 let express = require('express');
 let app = express();
 let mongoose = require('mongoose');
+let morgan = require('morgan');
 let router = express.Router();
-let Post = require('./models/post.js');
+let Message = require('./models/message.js');
 let bodyParser = require('body-parser');
-let allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-    next();
-}
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/api', router);
-
-app.use(allowCrossDomain);
 
 mongoose.connect(uristring);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
 
-});
-
-
-router.post('/chat/newMsg', function (req, res) {
-    let body = req.body;
-    let post = new Post({
-        author: body.author,
-        post: body.post
+router.post('/chat/newMsg', (req, res) => {
+    let {body: {author, post}} = req;
+    console.log(author,req.body, post);
+    let message = new Message({
+        author,
+        post
     });
-    post.save();
+    message.save();
     res.end('{"success" : "Posted Successfully", "status" : 200}');
 });
 
-router.get('/chat/messages', function (req, res) {
-    Post.find({}, function (err, users) {
-        if (err) {console.log(err,'hi');}
+router.get('/chat/messages', (req, res) => {
+    Message.find({}, (err, users) => {
+        if (err) {console.log(err);}
 
         res.status(200).send(users);
     });
 });
 
 
-app.listen(port, function () {
+app.listen(port, () => {
     console.log('listening on port: ' + port);
 });
