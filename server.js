b07aabf8ab660e0ef827a69ad7d9baef532bcf97
Jake Harclerode <jharclerode@walmartlabs.com>
2016-11-28 11:15:41 -0800

    refactor to use hapi

--- server.js
+++ server.js
@@ -1,54 +1,36 @@
 "use strict";
 const uristring = process.env.MONGODB_URI || 'mongodb://localhost/chatDB';
 const port = process.env.PORT || 3000;
-let express = require('express');
-let app = express();
+const host = !process.env.PORT && 'localhost';
+let Hapi = require('hapi');
+let routes = require('./routes.js');
 let mongoose = require('mongoose');
-let morgan = require('morgan');
-let router = express.Router();
 let Message = require('./models/message.js');
-let bodyParser = require('body-parser');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+let server = new Hapi.Server({
+    connections: {
+        routes: {
+            cors: true
+        }
+    }
+});
+server.connection({
+    port,
+    host
 });
 
-app.use(bodyParser.json());
-
-app.use('/api', router);
+server.route(routes);
 
 mongoose.connect(uristring);
 
 var db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
 
-router.post('/chat/newMsg', (req, res) => {
-    let {body: {author, post}} = req;
-    let message = new Message({
-        author,
-        post
-    });
-    message.save();
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
-        success : 'Posted Successfully',
-        status : 200,
-        _id: message._id,
-        createdOn: message.createdOn
-    }));
-});
+server.start(err => {
 
-router.get('/chat/messages', (req, res) => {
-    Message.find({}, (err, users) => {
-        if (err) {console.log(err);}
-
-        res.status(200).send(users);
-    });
-});
-
-
-app.listen(port, () => {
-    console.log('listening on port: ' + port);
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
 });
